8fc6cc1d5478fd3eac9f087d060d2f7ec44af13f
Jesse Yentsch <yentschj@gmail.com>
2019-01-26 16:51:52 -0500

    Removed timeSpent object

--- background.js
+++ background.js
@@ -4,16 +4,14 @@
 chrome.extension.onConnect.addListener(function(port){
     console.log("Connected....");
     var disconnectMes="Hey you disconnected!";
-    var totalTime={
-    };
+    
     port.onMessage.addListener(function(msg){
         console.log("message received " + msg);
         port.postMessage(disconnectMes);
-        totalTime=msg;
     });
     port.onDisconnect.addListener(function(){
         console.log("You haved siconnected on disconect");
-        alert("You have ended your timer! Don't procrastinate! " + totalTime.min + ":" + totalTime.sec);
+        alert("You have ended your timer! Don't procrastinate! " );
     });
 });
 
